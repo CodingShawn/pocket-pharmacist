1c5c4dde69b7ad417131548d25fb019da404a8a5
Shawn <codingshawn@gmail.com>
2021-05-26 20:59:57 +0800

    Created SearchForm component to search for a particular drug and store result in component state

---
+++ src/components/SearchForm.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import therapeuticProductsListingService from "../services/therapeuticProductsListing";
+
+function SearchForm() {
+  const [drugName, setDrugName] = useState("");
+  const [results, setResults] = useState(null);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    saveResultsToState();
+    setDrugName("");
+  }
+
+  async function saveResultsToState() {
+    let results = await therapeuticProductsListingService.getDrug(drugName);
+    setResults(results);
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        Drug name:{" "}
+        <input
+          type="text"
+          name="text"
+          onChange={({ target }) => setDrugName(target.value)}
+          value={drugName}
+        />
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  );
+}
+
+export default SearchForm;
