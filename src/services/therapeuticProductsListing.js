5b283d9e8c7c92a7666a78c88b4608273fff8595
Shawn <codingshawn@gmail.com>
2021-05-26 20:58:41 +0800

    Refactored therapeuticProductsListingService to return just the required array of results

--- src/services/therapeuticProductsListing.js
+++ src/services/therapeuticProductsListing.js
@@ -3,15 +3,17 @@ import axios from "axios";
 const BASE_URL =
   "https://data.gov.sg/api/action/datastore_search?resource_id=3ee20559-372d-42f0-bde9-245e21f7f39b";
 
-async function getDrug(drugName) {
+const therapeuticProductsListingService = {};
+
+therapeuticProductsListingService.getDrug = async function getDrug(drugName) {
   const request = {
     params: {
       q: { active_ingredients: drugName },
     },
   };
   const response = await axios.get(BASE_URL, request);
-  const { result } = response.data;
-  return result;
-}
+  const { records } = response.data.result;
+  return records;
+};
 
-export default { getDrug };
+export default therapeuticProductsListingService;
