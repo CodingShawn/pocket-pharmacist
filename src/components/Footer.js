13598065bed97f166c989c687fa318568eaa1c5f
Shawn <codingshawn@gmail.com>
2021-06-03 22:17:08 +0800

    Edited footer to include link of Singapore Open Data Licence within the text itself

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,11 +15,16 @@ function Footer() {
   ];
   const d = new Date();
   let todayDate = `${d.getDate()} ${months[d.getMonth()]} ${d.getFullYear()}`;
-  let attribution = `* Contains information from Listing of Registered Therapeutic Products accessed on ${todayDate} from Health Sciences Authority which is made available under the terms of the Singapore Open Data Licence version 1.0 https://data.gov.sg/open-data-licence`;
+  let attribution = `* Contains information from Listing of Registered Therapeutic Products accessed on ${todayDate} from Health Sciences Authority which is made available under the terms of the`;
 
   return (
     <footer id="footer">
-      <div>{attribution}</div>
+      <div>
+        {attribution}{" "}
+        <a href="https://data.gov.sg/open-data-licence">
+          Singapore Open Data Licence version 1.0
+        </a>
+      </div>
       <div>
         ** Data retrieved may be outdated. Refer to{" "}
         <a href="http://eservice.hsa.gov.sg/prism/common/enquirepublic/SearchDRBProduct.do?action=load">
