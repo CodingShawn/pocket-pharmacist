7ef99d5e1a80fcbad243af9f855b018395ca3bf9
Shawn <codingshawn@gmail.com>
2021-08-08 22:20:52 +0800

    Added unit testing for seperateComponents function

--- src/utils/utils.test.js
+++ src/utils/utils.test.js
@@ -15,7 +15,25 @@ describe("Testing of parseString function ", () => {
 
   test("parseString function parses string correctly with special characters removed", () => {
     expect(parser.parseString('"Adapalene&&Benzoyl peroxide"')).toBe(
-      "Adapalene, Benzoyl peroxide"
+      "ADAPALENE, BENZOYL PEROXIDE"
+    );
+  });
+});
+
+describe("Substituting commas with newlines in string", () => {
+  test("Replaces , with newline", () => {
+    expect(parser.seperateComponents("ORPHENADRINE CITRATE, PARACETAMOL")).toBe(
+      "ORPHENADRINE CITRATE\n PARACETAMOL"
+    );
+  });
+
+  test("Replaces multiple , with newlines", () => {
+    expect(
+      parser.seperateComponents(
+        "BETAMETHASONE DIPROPIONATE, CLOTRIMAZOLE, GENTAMICIN SULPHATE EQV GENTAMICIN"
+      )
+    ).toBe(
+      "BETAMETHASONE DIPROPIONATE\n CLOTRIMAZOLE\n GENTAMICIN SULPHATE EQV GENTAMICIN"
     );
   });
 });
