e391b3bd270291d6e0652f7952f400d33e3f54a4
Shawn <codingshawn@gmail.com>
2021-08-12 22:17:49 +0800

    Load all drug products info upon starting of app and pass data to form

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import DisplayResults from "./components/DisplayResults";
 import SearchForm from "./components/SearchForm";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import "./components.css";
+import therapeuticProductsListingService from "./services/therapeuticProductsListing"
 
 function App() {
   const [results, setResults] = useState(null);
   const [searchTerm, setSearchTerm] = useState("");
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    setDrugData()
+  }, [])
+
+  async function setDrugData() {
+    const drugs = await therapeuticProductsListingService.getAllDrugs();
+    setData(drugs);
+  }
 
   return (
     <>
       <Header />
-      <SearchForm setResults={setResults} setSearchTerm={setSearchTerm} />
+      <SearchForm setResults={setResults} setSearchTerm={setSearchTerm} data={data} />
       {results && <DisplayResults results={results} searchTerm={searchTerm} />}
       <Footer />
     </>
